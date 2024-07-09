fcccc4a24c4f5bead59f8b6520ead479384b7aa4
league <yt>
2024-07-09 11:56:10 -0700

    July 9th 2024

--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,30 +14,31 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    noggin=turtle.Turtle
+    noggin=turtle.Turtle()
     
     # This code sets our shape to a turtle
-    noggin.shape(turtle)
-    # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
+    noggin.shape('turtle')
+   # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
     noggin.speed(10)
     
     # Set your turtle's color using .color('green')
     noggin.color('green')
     
     # Use a loop to repeat a the code below 50 times
-    for i in range (50)
+    for i in range(50):
         
         # Set the turtle color to a random color
-        noggin.color()
+        noggin.color('red')
 
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-         noggin.forward(5*i)
+        noggin.forward(5*i)
 
         # Turn the turtle (360/7) degrees to the right
-    noggin.right (360/7)
+        noggin.right(360/7)
          
         # Change the turtle width to 'i' (the loop variable)
-        
+        noggin.width(i)
+
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,46 +21,64 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
+    kit=turtle.Turtle()
     
     # Make the turtle shape 'turtle', .shape('turtle')
+    kit.shape('turtle')
     
     # Set the turtle width to 2
+    kit.width(2)
     
     # Set the turtle speed to 0 (fastest)
+    kit.speed(0)
     
     # Use a for loop to repeat all of the code below ONE time (we will change
-    # this later)
+    # this later
+    for i in range(25):
         
         # Set the turtle .fillcolor() to orange
+        kit.fillcolor('orange')
         
         # Call the turtle .begin_fill() function
+        kit.begin_fill()
         
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
+        kit.right(360)
         #                will turn a full circle)
         
         # DRAW           Move the turtle 64 pixels
+        kit.forward(64)
         
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
+        kit.left(40)
         #                numbers will turn the turtle counter-clockwise.)
         
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
+        kit.forward(flameSize)
         
         #                Turn the turtle to the right 170 degrees
+        kit.right(170)
          
         #                Move the turtle the distance in the variable flameSize (again)
+        kit.forward(flameSize)
          
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
+        kit.right(62)
         
         #  DRAW          Move the turtle the distance in the variable baseSize
+        kit.forward(baseSize)
         
         # Call the turtle .end_fill() method
+        kit.end_fill()
         
     # Hide your turtle so you can see the pattern.
+    kit.hideturtle
         
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
     # COLOR  Change the turtle's pen color so that the flame is a different
+    kit.pencolor('green')
     #        color to the rest of the star. Run the program again. Check the
     #        second picture in the recipe.
 
