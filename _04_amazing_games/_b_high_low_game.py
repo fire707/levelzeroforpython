270c965ab630dcc8ea10e2e079830c732b61b1c5
league <yt>
2024-07-12 10:59:10 -0700

    July 12th 2024

--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -13,18 +13,31 @@ if __name__ == '__main__':
     print(random_num)
 
     # 3. Code a for loop to run steps 4-10, 10 times
-    if i in range(10)
+    for i in range(10):
 
-        # 4. Ask the user for a guess using a pop-up window, and save their response
+        dig=simpledialog.askstring(title='Greeter',prompt="Give me a guess")
 
-        # 5. If the guess is correct
+    # 5. If the guess is correct
+        if dig=='5':
+            messagebox.showinfo(message="won")
+            syst.exit(0)
+
+
             # 6. Win. Use 'sys.exit(0)' to end the program
 
         # 7. if the guess is high
+        if dig>'5':
+            messagebox.showinfo(message="you guess to high")
             # 8. Tell them it's too high
+
         # 9. Else if the guess is low
+        if dig<'5':
+            messagebox.showinfo(message="you guessed to low")
+
+
             # 10. Tell them it's too low
 
     #11. Outside of the loop, tell the user they lost
+messagebox.showinfo(message="I guess you lost")
 
-    window.mainloop()
+window.mainloop()
